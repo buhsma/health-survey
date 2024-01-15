c044666d1ebec4014fc64b19fa51c8aa96dc692c
[buhsma] <[ismael.buehler@gmail.com]>
2024-01-15 16:25:59 +0100

    header start

--- www/public/src/components/templates/header.php
+++ www/public/src/components/templates/header.php
@@ -2,7 +2,16 @@
     
 
 <header>
-
+        <div class="header__leftBox"></div>
+        <div class="btnContainer">
+            <button class="btn back" role="button">
+                <span class="text">Fit&Well</span>
+            </button>
+            <button class="btn back" role="button">
+                <span class="text">Reset Survey</span>
+            </button>
+        </div>
+        <div class="header__rightBox"></div>
 </header>
 
 <div class="grid">
