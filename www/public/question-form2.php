936c1a911e4d13f7749953900970fec6fe6eca97
rgrttg <ottiger@outlook.de>
2024-01-15 16:47:53 +0100

    fyrabig

--- www/public/question-form2.php
+++ www/public/question-form2.php
@@ -6,9 +6,8 @@ include './src/components/templates/header.php';
 
 <main>
   <form class="form" action="question-form2.php" method="POST">
-    <div class="form__questionField">
+    <div class="form__questionField">      <!-- Check Boxes -->
       <p class="form__question__text">What additional physical activity do you do most?</p>
-      <!-- Check Boxes -->
       <input id="q41" type="checkbox" name="q41" value="" />
       <label for="q41">Lifting weights</label><br>
       <input id="q42" type="checkbox" name="q42" value="no" />
@@ -30,15 +29,12 @@ include './src/components/templates/header.php';
       <input id="q410" type="checkbox" name="q410" value="yes" />
       <label for="q410">Other</label><br>
       </div>
-<hr>
-
-    <div class="form__questionField">
+    <div class="form__questionField">      <!-- Slider -->
       <p class="form__question__text">Do you feel you do too little, just enough or way too much additional physical activity?</p>
       <label for="q5">Far too little / just right / far too much</label>
-      <!-- Slider -->
       <input id="q5" type="range" min=0 max=10 name="q5" />
     </div>
-    <a href="question-form1.php">Prev </a><a href="question-form3.php"> Next</a>
+    <a href="question-form1.php">Prev</a><br><a href="question-form3.php">Next</a>
   </form>
 </main>
 
