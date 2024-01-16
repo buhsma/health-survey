9d734ae691f8c86c70bac6903ed8b401d0388717
[buhsma] <[ismael.buehler@gmail.com]>
2024-01-16 11:31:15 +0100

    php-validations-start

--- www/public/src/components/validations.php
+++ www/public/src/components/validations.php
@@ -1,25 +1,61 @@
-   
+<?php
 
 
+// if($_SERVER['REQUEST_METHOD'] === 'POST') {
+//   if (($_POST["q1"]) < 0)
+//     {
+//       $q1 = $_POST['q1'];
+//       resultsArrFunk($q1);
+//     } 
+//     else 
+//     {
+//       $errMessage = "Gimme input";
+//     }
+// } 
+
+if (!isset($_SESSION['results'])) {
+  $_SESSION['results'] = [];
+}
+
+//q1
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST["q1"]) && $_POST["q1"] > 0) {
+      $q1 = $_POST['q1'];
+      resultsArrFunk('q1');
+  }
+  else {
+    $errMessage = "q1 nothappy";
+  }
+}
+
+//q2
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST["q2"])) {
+    resultsArrFunk('q2');
+  }
+}
 
-} 
-
-if (($_POST["q1"]) < 0)
-    {
-      $ageErr = "Customers must be aged 16-90";
-    } 
-    else 
-    {
-      $rangeSlider = test_input($_POST["ageOutput"]);
-    }
-
-
+//q3
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($_POST["q3"]) && $_POST["q3"] > 0) {
+      $q1 = $_POST['q3'];
+      resultsArrFunk('q3');
+  }
+  else {
+    $errMessage = "q3 nothappy";
+  }
+}
 
 
 
 
+function resultsArrFunk($key) {
+  global $$key;
 
+  if (isset($$key)) {
+      $_SESSION['results'][$key] = $$key;
+  }
+}
 
 
 
