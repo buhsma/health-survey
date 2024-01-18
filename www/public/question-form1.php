39ce1e7f6c92ab599c4e517c34922c389a194229
rgrttg <ottiger@outlook.de>
2024-01-18 11:53:10 +0100

    flex direction row

--- www/public/question-form1.php
+++ www/public/question-form1.php
@@ -9,17 +9,21 @@ include './src/components/templates/header.php';
   <form class="form" action="./question-form2.php" method="POST" id="form1">
     <div class="form__questionField">      <!-- Check Radio -->
       <p class="form__question__text">Do you take nutritional supplements?</p>
-      <label for="q2y">Yes</label>
-      <input id="q2y" type="radio" value="yes" name="q2">
-      <label for="q2n">No</label>
-      <input id="q2n" type="radio" value="no" name="q2" checked>
+      <div class="form__questionField__input">
+        <input id="q2y" type="radio" value="yes" name="q2">
+        <label for="q2y">Yes</label>
+      </div>
+      <div class="form__questionField__input">
+        <input id="q2n" type="radio" value="no" name="q2">
+        <label for="q2n">No</label>
+      </div>
       <!-- Warn-Hinweis -->
       <p id="warning2" class="validation-warning"></p>
     </div>
     <div class="form__questionField">      <!-- Slider -->
       <p class="form__question__text">How important is physical activity to you?</p>
       <label for="q3">0 / Not at all - Very much</label>
-      <input id="q3" type="range" min="0" max="10" step="1" value="0" name="q3" />
+      <input id="q3" type="range" min="0" max="5" step="1" value="0" name="q3" />
       <!-- Warn-Hinweis -->
       <p id="warning3" class="validation-warning"></p>
     </div>
--- www/public/question-form2.php
+++ www/public/question-form2.php
@@ -12,41 +12,52 @@ include './src/components/templates/header.php';
       <!-- <label for="q40">None</label>
       <input id="q40" type="checkbox" name="q4[0]" value="none" />
       <br> -->
-      <label for="q41">Lifting weights</label>
-      <input id="q41" type="checkbox" name="q4[1]" value="lifting-weights" />
-      <br>
-      <label for="q42">Walking</label>
-      <input id="q42" type="checkbox" name="q4[2]" value="walking" />
-      <br>
-      <label for="q43">Jogging</label>
-      <input id="q43" type="checkbox" name="q4[3]" value="jogging" />
-      <br>
-      <label for="q44">Running</label>
-      <input id="q44" type="checkbox" name="q4[4]" value="running" />
-      <br>
-      <label for="q45">Swimming</label>
-      <input id="q45" type="checkbox" name="q4[5]" value="swimming" />
-      <br>
-      <label for="q46">Dancing</label>
-      <input id="q46" type="checkbox" name="q4[6]" value="dancing" />
-      <br>
-      <label for="q47">Aerobics</label>
-      <input id="q47" type="checkbox" name="q4[7]" value="aerobics" />
-      <br>
-      <label for="q48">Pilates</label>
-      <input id="q48" type="checkbox" name="q4[8]" value="pilates" />
-      <br>
-      <label for="q49">Team sports</label>
-      <input id="q49" type="checkbox" name="q4[9]" value="team-sports" />
-      <br>
-      <label for="q410">Other</label>
-      <input id="q410" type="checkbox" name="q4[10]" value="other" />
-      <p id="warning4" class="validation-warning"></p>
+      <div class="form__questionField__input">
+        <input id="q41" type="checkbox" name="q4[1]" value="lifting-weights" />
+        <label for="q41">Lifting weights</label>
+      </div>
+      <div class="form__questionField__input">
+        <input id="q42" type="checkbox" name="q4[2]" value="walking" />
+        <label for="q42">Walking</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q43" type="checkbox" name="q4[3]" value="jogging" />
+        <label for="q43">Jogging</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q44" type="checkbox" name="q4[4]" value="running" />
+        <label for="q44">Running</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q45" type="checkbox" name="q4[5]" value="swimming" />
+        <label for="q45">Swimming</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q46" type="checkbox" name="q4[6]" value="dancing" />
+        <label for="q46">Dancing</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q47" type="checkbox" name="q4[7]" value="aerobics" />
+        <label for="q47">Aerobics</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q48" type="checkbox" name="q4[8]" value="pilates" />
+        <label for="q48">Pilates</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q49" type="checkbox" name="q4[9]" value="team-sports" />
+        <label for="q49">Team sports</label>
+      </div> 
+      <div class="form__questionField__input">
+        <input id="q410" type="checkbox" name="q4[10]" value="other" />
+        <label for="q410">Other</label>
+        <p id="warning4" class="validation-warning"></p>
+      </div>
     </div>
     <div class="form__questionField">      <!-- Slider -->
       <p class="form__question__text">Do you feel you do too little, just enough or way too much additional physical activity?</p>
       <label for="q5">0 / Far too little - just right - far too much</label>
-      <input id="q5" type="range" min="0" max="10" value="0" name="q5" />
+      <input id="q5" type="range" min="0" max="5" value="0" name="q5" />
       <p id="warning5" class="validation-warning"></p>
     </div>
     <div class="form__questionField">
